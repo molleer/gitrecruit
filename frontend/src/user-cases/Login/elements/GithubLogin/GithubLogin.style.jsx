144181b72610148d380c5a4e711f542789f3e6e8
David Möller <davidm@student.chalmers.se>
2020-08-06 20:01:49 +0200

    Created a github login button

---
+++ frontend/src/user-cases/Login/elements/GithubLogin/GithubLogin.style.jsx
@@ -0,0 +1,15 @@
+import Styled from 'styled-components';
+
+export const Button = Styled.button`
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 0.5rem 2rem 0.5rem 2rem;
+  display: flex;
+  align-items: center;
+`;
+
+export const IconWrapper = Styled.div`
+  padding-right: 1rem;
+`;
