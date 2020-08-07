c2b53c7bf64647a6a86488e07501d91ef78ed98a
David Möller <davidm@student.chalmers.se>
2020-08-08 01:04:29 +0200

    Checking if user is logged in when loading page

--- frontend/src/common/providers/User.provider.jsx
+++ frontend/src/common/providers/User.provider.jsx
@@ -1,9 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import UserContext from '../contexts/User.context';
 import PropTypes from 'prop-types';
+import Axios from 'axios';
 
 const UserProvider = ({children}) => {
   const [user, setUser] = useState(null);
+  useEffect(() => {
+    Axios.get('/api/login/checkLogin')
+      .then((res) => setUser(res.data))
+      .catch((err) => setUser(null));
+  }, []);
   return (
     <UserContext.Provider value={[user, setUser]}>
       {children}
